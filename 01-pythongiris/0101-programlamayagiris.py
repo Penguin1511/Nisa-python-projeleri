6847b3a814a6388b2c9bc56408c46330d4595197
penguin1511 <nisainan1511@gmail.com>
2022-09-10 11:13:03 +0300

    10092022-11:12

---
+++ 01-pythongiris/0101-programlamayagiris.py
@@ -0,0 +1,9 @@
+# print("merhaba dünya")
+# print("Haftanın 1. günü\t: Pazartesi\nHaftanın 2. günü\t: Salı")
+# print("S.N.\t Ad\t Soyad \n ---\t ---\t ---\n 1\t Yusuf\t Şahin")
+"""
+Dünyanın♥️En♥️Güzel♥️Şehri→İstanbul.
+alt + 3 ♥️
+alt + 26 →
+"""
+print(((1+(14/2))**(1/3))-15/(1+((2**3)/2)))
